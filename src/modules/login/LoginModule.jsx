390719879bcd564cb9fadfdaab43dc00ca00f828
unknown <annabelen.ra@gmail.com>
2023-08-13 18:03:27 -0300

    optimizacion de componente

--- src/modules/login/LoginModule.jsx
+++ src/modules/login/LoginModule.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
 import * as Yup from "yup";
 import "./style/login-module.css";
 import { Button } from "../../components";
@@ -6,74 +6,94 @@ import { useGlobalContext } from "../../context/storeContext";
 import { useNavigate } from "react-router-dom";
 
 export const LoginModule = () => {
-  const { setRolAdmin, setIsRol} = useGlobalContext();
+  const { setRolAdmin, setIsRol } = useGlobalContext();
   const navigate = useNavigate();
-  const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .email("Email inválido")
-      .required("El email es obligatorio"),
-    password: Yup.string()
-      .min(6, "La contraseña debe tener al menos 6 caracteres")
-      .required("La contraseña es obligatoria"),
-    role: Yup.string()
-      .required("Selecciona un rol")
-      .oneOf(["admin", "user"], "Rol inválido"),
+  const form = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      role: "",
+      },
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email("Email inválido")
+        .required("El email es obligatorio"),
+      password: Yup.string()
+        .min(6, "La contraseña debe tener al menos 6 caracteres")
+        .required("La contraseña es obligatoria"),
+      role: Yup.string()
+        .required("Selecciona un rol")
+        .oneOf(["admin", "user"], "Rol inválido"),
+    }),
+    onSubmit: (values, {resetForm}) => {
+      resetForm()
+      if (values.role=== "admin") {
+        setRolAdmin(true);
+      } else {
+        setRolAdmin(false);
+      }
+      setIsRol(true);
+      navigate ("/home");
+    },
   });
+
   return (
     <div className="container">
       <div className="background"></div>
       <div className="container-form">
-        <Formik
-          initialValues={{ email: "", password: "", role: "admin" }}
-          validationSchema={validationSchema}
-          onSubmit={(values) => {
-              // se debe hacer peticion al servidor para autenticar, en este caso lo simulamos
-            if (values.role=== "admin") {
-              setRolAdmin(true);
-            } else {
-              setRolAdmin(false);
-            }
-            setIsRol(true);
-            navigate ("/home");
-          }}
-        >
-          <Form>
-            <div>
-              <label className="label" htmlFor="email">
-                Email:
-              </label>
-              <Field className="input" type="email" id="email" name="email" />
-              <ErrorMessage name="email" component="div" className="error" />
-            </div>
+        <form onSubmit={form.handleSubmit}>
+          <div>
+            <label className="label" htmlFor="email"></label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              onChange={form.handleChange}
+              value={form.values.email}
+            />
+            <p className="error">{form.errors.email}</p>
+          </div>
+          <div>
+            <label className="label" htmlFor="password">
+              Password:
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              onChange={form.handleChange}
+              value={form.values.password}
+            />
+            <p className="error">{form.errors.password}</p>
+          </div>
 
-            <div>
-              <label className="label" htmlFor="password">
-                Password:
+          <div>
+            <div className="container-check">
+              <label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="admin"
+                  checked={form.values.role === 'admin'}
+                  onChange={form.handleChange}
+                />
+                Admin
               </label>
-              <Field
-                className="input"
-                type="password"
-                id="password"
-                name="password"
-              />
-              <ErrorMessage name="password" component="div" className="error" />
+              <label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="user"
+                  checked={form.values.role === 'user'}
+                  onChange={form.handleChange}
+                />
+                User
+              </label>
             </div>
-
-            <div>
-              <div className="container-check">
-                <label>
-                  <Field type="radio" name="role" value="admin" id="check" />{" "}
-                  Admin
-                </label>
-                <label>
-                  <Field type="radio" name="role" value="user" /> User
-                </label>
-              </div>
-              <ErrorMessage name="role" component="div" className="error" />
-            </div>
-            <Button text="Enviar" type="primary" />
-          </Form>
-        </Formik>
+            <p className="error">{form.errors.role}</p>
+          </div>
+          <Button text="Enviar" type="primary" />
+        </form>
       </div>
     </div>
   );
